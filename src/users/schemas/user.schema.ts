a2df3bc331420a92c7ef25ecf7a8b7cd95f7aca1
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-07 12:56:41 +0700

    Create User

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -6,13 +6,13 @@ export type UserDocument = HydratedDocument<User>;
 @Schema()
 export class User {
   @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
-
-  @Prop({ required: true })
-  name: string;
 
   @Prop({ required: true })
   phone: number;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+// import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  // create(createUserDto: CreateUserDto) {
+  async create(
+    name: string,
+    email: string,
+    password: string,
+    phone: number,
+    age: number,
+    address: string,
+  ) {
+    return await this.userModel.create({
+      name,
+      email,
+      password,
+      phone,
+      age,
+      address,
+    });
   }
 
   findAll() {
