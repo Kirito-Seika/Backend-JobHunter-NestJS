566c9e3df2fdf3a0d8145244c711c9c87bb52b53
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-07 11:38:28 +0700

    Create Schema (Model)

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  phone: number;
+
+  @Prop({ required: true })
+  age: number;
+
+  @Prop({ required: true })
+  address: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+}
+export const UserSchema = SchemaFactory.createForClass(User);
