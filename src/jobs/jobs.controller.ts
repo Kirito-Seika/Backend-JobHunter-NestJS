33f2ab5e0e193d4936f526899a7726c0e887d497
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-23 03:50:14 +0700

    CRUD Jobs

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -6,37 +6,53 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
-import { JobsService } from './jobs.service';
-import { CreateJobDto } from './dto/create-job.dto';
-import { UpdateJobDto } from './dto/update-job.dto';
+import { JobsService } from 'src/jobs/jobs.service';
+import { CreateJobDto } from 'src/jobs/dto/create-job.dto';
+import { UpdateJobDto } from 'src/jobs/dto/update-job.dto';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/types/user.interface';
 
 @Controller('jobs')
 export class JobsController {
   constructor(private readonly jobsService: JobsService) {}
 
   @Post()
-  create(@Body() createJobDto: CreateJobDto) {
-    return this.jobsService.create(createJobDto);
+  @ResponseMessage('Create Job')
+  create(@Body() createJobDto: CreateJobDto, @User() user: IUser) {
+    return this.jobsService.create(createJobDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  @ResponseMessage('Fetch All Job')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
+    @Query() reqString: string,
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, reqString);
   }
 
   @Get(':id')
+  @ResponseMessage('Fetch Job By Id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
-    return this.jobsService.update(+id, updateJobDto);
+  @ResponseMessage('Update Job')
+  update(
+    @Param('id') id: string,
+    @Body() updateJobDto: UpdateJobDto,
+    @User() user: IUser,
+  ) {
+    return this.jobsService.update(id, updateJobDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.jobsService.remove(+id);
+  @ResponseMessage("Delete Job")
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.jobsService.remove(id, user);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,26 +1,118 @@
 import { Injectable } from '@nestjs/common';
-import { CreateJobDto } from './dto/create-job.dto';
-import { UpdateJobDto } from './dto/update-job.dto';
+import { CreateJobDto } from 'src/jobs/dto/create-job.dto';
+import { UpdateJobDto } from 'src/jobs/dto/update-job.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/types/user.interface';
+import aqp from 'api-query-params';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class JobsService {
-  create(createJobDto: CreateJobDto) {
-    return 'This action adds a new job';
+  constructor(
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>,
+  ) {}
+
+  async create(createJobDto: CreateJobDto, user: IUser) {
+    const {
+      name,
+      skills,
+      company,
+      salary,
+      quantity,
+      level,
+      description,
+      startDate,
+      endDate,
+      isActive,
+    } = createJobDto;
+    let newJob = await this.jobModel.create({
+      name,
+      skills,
+      company,
+      salary,
+      quantity,
+      level,
+      description,
+      startDate,
+      endDate,
+      isActive,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
+    return {
+      _id: newJob?._id,
+      createdAt: newJob?.createdAt,
+    };
   }
 
-  findAll() {
-    return `This action returns all jobs`;
+  async findAll(currentPage: number, limit: number, reqString: string) {
+    const { filter, sort, population } = aqp(reqString);
+    delete filter.current;
+    delete filter.pageSize;
+
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.jobModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.jobModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return `Not Found Job`;
+    }
+    return this.jobModel.findById(id);
   }
 
-  update(id: number, updateJobDto: UpdateJobDto) {
-    return `This action updates a #${id} job`;
+  update(_id: string, updateJobDto: UpdateJobDto, user: IUser) {
+    return this.jobModel.updateOne(
+      { _id },
+      {
+        ...updateJobDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} job`;
+  async remove(_id: string, user: IUser) {
+    if (!mongoose.Types.ObjectId.isValid(_id)) return `Not Found Job`;
+    await this.jobModel.updateOne(
+      { _id },
+      {
+        deletedBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.jobModel.softDelete({
+      _id,
+    });
   }
 }
