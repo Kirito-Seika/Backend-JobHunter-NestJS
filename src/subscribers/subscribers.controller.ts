d42bb9686eca3c5d86bcfff7634b1e7d46e1233e
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-26 05:09:00 +0700

    CRUD Subscribers

--- src/subscribers/subscribers.controller.ts
+++ src/subscribers/subscribers.controller.ts
@@ -1,34 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { SubscribersService } from 'src/subscribers/subscribers.service';
 import { CreateSubscriberDto } from 'src/subscribers/dto/create-subscriber.dto';
 import { UpdateSubscriberDto } from 'src/subscribers/dto/update-subscriber.dto';
+import { ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/types/user.interface';
 
 @Controller('subscribers')
 export class SubscribersController {
   constructor(private readonly subscribersService: SubscribersService) {}
 
   @Post()
-  create(@Body() createSubscriberDto: CreateSubscriberDto) {
-    return this.subscribersService.create(createSubscriberDto);
+  @ResponseMessage("Create a subscriber")
+  create(@Body() createSubscriberDto: CreateSubscriberDto, @User() user: IUser) {
+    return this.subscribersService.create(createSubscriberDto, user);
   }
 
   @Get()
-  findAll() {
-    return this.subscribersService.findAll();
+  @ResponseMessage("Fetch subscribers with paginate")
+  findAll(
+    @Query("current") currentPage: string,
+    @Query("pageSize") limit: string,
+    @Query() reqString: string,
+  ) {
+    return this.subscribersService.findAll(+currentPage, +limit, reqString);
   }
 
   @Get(':id')
+  @ResponseMessage("Fetch subscriber by id")
   findOne(@Param('id') id: string) {
-    return this.subscribersService.findOne(+id);
+    return this.subscribersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSubscriberDto: UpdateSubscriberDto) {
-    return this.subscribersService.update(+id, updateSubscriberDto);
+  @ResponseMessage("Update a subscriber")
+  update(
+    @Param('id') id: string,
+    @Body() updateSubscriberDto: UpdateSubscriberDto,
+    @User() user: IUser
+  ) {
+    return this.subscribersService.update(id, updateSubscriberDto, user);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.subscribersService.remove(+id);
+  @ResponseMessage("Delete a subscriber")
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.subscribersService.remove(id, user);
   }
 }
