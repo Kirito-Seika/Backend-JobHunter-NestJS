8e3fc1a893535a9118ab952f452e19903a78f2f8
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-01-27 22:57:20 +0700

    helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory, Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { TransformInterceptor } from 'src/core/transform.interceptor';
 import { AppModule } from 'src/app.module';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -42,6 +43,9 @@ const bootstrap = async () => {
     type: VersioningType.URI,
     defaultVersion: ['1'],
   });
+
+  //config helmet
+  app.use(helmet());
 
   const port = configService.get<string>('PORT');
   await app.listen(port);
